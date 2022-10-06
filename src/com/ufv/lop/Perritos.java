2d37d969b2687ec8c48cc2f7f6c350937e07201c
PabloC <7736022@alumnos.ufv.es>
2022-10-06 20:04:00 +0200

    Añadida clase perritos a main

---
+++ src/com/ufv/lop/Perritos.java
@@ -0,0 +1,11 @@
+package com.ufv.lop;
+
+public class Perritos {
+    public String getRaza(){
+        return raza;
+    }
+    public void setRaza(String _raza){
+        this.raza = _raza;
+    }
+    private String raza;
+}
